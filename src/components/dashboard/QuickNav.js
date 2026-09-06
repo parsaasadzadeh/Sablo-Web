4104b5eabcb10e43ba98c5b138b21690b3435b44
parsaasadzadeh <fightingparsa@gmail.com>
2026-09-06 23:55:26 +0330

    Update QuickNav.js

--- src/components/dashboard/QuickNav.js
+++ src/components/dashboard/QuickNav.js
@@ -1,7 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { BarChart2, Target , ArrowLeftRigh,Landmark} from "lucide-react";
-
+import { BarChart2, Target, ArrowLeftRight, Landmark } from "lucide-react";
 export default function QuickNav() {
   const router = useRouter();
 
